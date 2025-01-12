0c84b633e6681a9987f71712a316a5da05a9b720
Andrew <50636418+Andrewbandrew05@users.noreply.github.com>
2025-01-12 17:50:13 -0500

    added some basic server files

---
+++ 0 Wireless Measurement Delivery/server.py
@@ -0,0 +1,30 @@
+import socket
+import threading
+import time
+
+# receive data from the client
+def handleClient(client_socket):
+    while True:
+        client_socket.send("test".encode("utf-8"))
+        time.sleep(1)
+
+# create a socket object
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_ip = "127.0.0.1"
+port = 8000
+
+# bind the socket to a specific address and port
+server.bind((server_ip, port))
+
+# listen for incoming connections
+server.listen(20)
+print(f"Listening on {server_ip}:{port}")
+
+while True:
+    # accept incoming connections
+    client_socket, client_address = server.accept()
+    thread = threading.Thread(target=handleClient, args=(client_socket,))
+    thread.start()
+    print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
+
