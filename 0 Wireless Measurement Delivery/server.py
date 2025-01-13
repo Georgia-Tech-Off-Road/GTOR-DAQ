4dc746f08d95dc302fbd2efc72578012f7130f8e
Andrew <50636418+Andrewbandrew05@users.noreply.github.com>
2025-01-13 13:45:54 -0500

    added a couple debugging things

--- 0 Wireless Measurement Delivery/server.py
+++ 0 Wireless Measurement Delivery/server.py
@@ -2,6 +2,17 @@ import socket
 import threading
 import time
 
+global currentData
+
+#function to create test data
+def read_File(filename):
+    file = open(filename)
+    while True:
+        currentData = file.readline()
+        time.sleep(1)
+        if currentData == "":
+            break
+
 # receive data from the client
 def handleClient(client_socket):
     while True:
@@ -20,6 +31,10 @@ server.bind((server_ip, port))
 # listen for incoming connections
 server.listen(20)
 print(f"Listening on {server_ip}:{port}")
+
+#create test thread to fead data from a file to clients
+testDataThread = threading.Thread(target=handleClient, args=(PUTFILENAMEHERE,))
+testDataThread.start()
 
 while True:
     # accept incoming connections
