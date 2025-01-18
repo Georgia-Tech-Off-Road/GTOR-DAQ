b86c080ffda29fa2f94bb2bee3659b656c38f227
Andrew <50636418+Andrewbandrew05@users.noreply.github.com>
2025-01-18 16:29:52 -0500

    ADCS ARE WORKING!
    I got an ADS1115 ADC to work with the aux daq. It uses interupts to alert the teensy to when data is ready which is then collected and saved in an array of analog values. These values are then  pasted into the output file

--- 5 DAQ Pack/DaqPackCode/DaqPackCode.ino
+++ 5 DAQ Pack/DaqPackCode/DaqPackCode.ino
@@ -2,10 +2,14 @@
 //Note: Also could maybe decrease bit resolution
 //Note: Should get an external powersupply to get better VCC stability
 //sd card stuff
+
+//try to change this to use interupts instead of constantly checking each pin (there's apparently a seperate piece of hardware that geenrates the interupt signal so using interupts should save clock cycles)
 #include "SD.h"
 #include "SPI.h"
 //time stuff
 #include <TimeLib.h>
+#include <Adafruit_ADS1X15.h>
+#include <Wire.h>
 
 #define BAUD 230400
 
@@ -14,6 +18,17 @@
 File outputFile;
 
 bool isRecording = false;
+
+//the index in this matches up to the index of binary data in the printf
+int binaryValues[3] = {0, 0, 0};
+
+Adafruit_ADS1115 ads;
+//stores current values for sensors connected to adc0
+int analogValues[4] = {0, 0, 0, 0};
+//current analog sensor number being polled
+int currentAnalogSensor = 0;
+
+int lastSaveTimeInSeconds = 0;
 
 void setup() {
   // set the Time library to use Teensy 3.0's RTC to keep time
@@ -34,56 +49,106 @@ void setup() {
   //sets top leds to output
   pinMode(9, OUTPUT); //red LED (recording)
   digitalWrite(8, HIGH); //turn on white LED
-  //pulls down signals on inputs
-  pinMode(7, INPUT_PULLDOWN);
-  pinMode(20, INPUT_PULLDOWN); // rear diff
-  pinMode(21, INPUT_PULLDOWN); // front left halleffect
-  pinMode(22, INPUT_PULLDOWN); // front right halleffect
+  pinMode(7, INPUT_PULLDOWN); //pull down input for record stop/start button (NEEDED TO MAKE IT WORK)
+  //set the values in the digital value array to their initial values (likely doesn't matter but feels better to do it this way)
+  binaryValues[0] = digitalRead(20);
+  binaryValues[1] = digitalRead(21);
+  binaryValues[2] = digitalRead(22);
+  //sets up interupts for binary values
+  attachInterrupt(digitalPinToInterrupt(7), changeRecordingState, CHANGE); //record button
+  attachInterrupt(digitalPinToInterrupt(20), updateRearDiff, CHANGE); //rear diff
+  attachInterrupt(digitalPinToInterrupt(21), updateFrontLeftHalleffect, CHANGE); // front left halleffect
+  attachInterrupt(digitalPinToInterrupt(22), updateFrontRightHalleffect, CHANGE); // front right halleffect
+  //sets up interupt pin for ADS1115 ADC (has to pullup alert pin in accordance with ADS1115 datasheet)
+  pinMode(15, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(15), readAnalogValues, FALLING);
+  //configure ADC for continuous operation 
+  ads.begin();
+  //set data rate to max 
+  ads.setDataRate(RATE_ADS1115_860SPS);
   pinMode(17, INPUT_PULLDOWN); // front brake pressure
   pinMode(16, INPUT_PULLDOWN); //rear brake pressure
   isRecording = true;
   digitalWrite(9, HIGH); //turn on red LED
-  outputFile.println(now());
+  //start first ADC reading to begin the cycle
+  ads.startADCReading(ADS1X15_REG_CONFIG_MUX_SINGLE_0, false);
 }
-//writes data to SD card and then checks if it should stop recording
+//writes data to SD card
 void loop() {
   //try testing with an oscilloscope
   //potentially reduce print buffer time?
   //use flush?
-  outputFile.printf("%llu,%lu,%d,%d,%d,%d,%d\n", now(), micros()
-  , digitalRead(20), digitalRead(21), digitalRead(22), analogRead(17), analogRead(16));
-  //Serial.printf("%llu,%lu,%d,%d,%d,%d,%d\n", now(), micros()
-  //, digitalRead(17), digitalRead(20), digitalRead(21), analogRead(22), analogRead(23));
-  if (digitalRead(7) == 1) {
-    if(isRecording == true) {
-      while(digitalRead(7) == 1) {
-        Serial.println("RELEASE BUTTON");
-      }
-      delay(300); //delays to prevent debounce
-      outputFile.println(now());
-      outputFile.close();
-      digitalWrite(9, LOW); //turn off red LED
-      Serial.println("Data Recording Stopped");
-     
-      isRecording = false;
-    }
-    else {
-      while(digitalRead(7) == 1) {
-        Serial.println("RELEASE BUTTON");
-      }
-      delay(300); //delays to prevent debounce
-      String time =  String(year()) + "-" + String(month()) + "-" + String(day()) + " " + String(hour()) + "_" + String(minute()) + "_" + String(second());
-      Serial.println(time.c_str());
-      //turn on red LED
-      digitalWrite(9, HIGH);
-      outputFile = SD.open(time.c_str(),  FILE_WRITE);
-      outputFile.println(now());
-      isRecording = true;
-    }
-  }
+  outputFile.printf("%llu,%lu,%d,%d,%d,%d,%d,%d,%d\n", now(), micros()
+  , binaryValues[0], binaryValues[1], binaryValues[2], analogValues[0], analogValues[1], analogValues[2], analogValues[3]);
+  //Serial.printf("%llu,%lu,%d,%d,%d,%d,%d,%d,%d\n", now(), micros()
+  //, binaryValues[0], binaryValues[1], binaryValues[2], analogValues[0], analogValues[1], analogValues[2], analogValues[3]);
 }
 //method needed to get time
 time_t getTeensy3Time()
 {
   return Teensy3Clock.get();
+}
+//I think you need a seperate one for some reason :(
+void updateRearDiff() {
+  binaryValues[0] = !binaryValues[0];
+}
+void updateFrontLeftHalleffect() {
+  binaryValues[1] = !binaryValues[1];
+}
+void updateFrontRightHalleffect() {
+  binaryValues[2] = !binaryValues[2];
+}
+void readAnalogValues() {
+  switch (currentAnalogSensor) {
+      case 0:
+        analogValues[0] =  ads.getLastConversionResults();
+        currentAnalogSensor = 1;
+        ads.startADCReading(ADS1X15_REG_CONFIG_MUX_SINGLE_1, false);
+        break;
+      case 1:
+        analogValues[1] =  ads.getLastConversionResults();
+        currentAnalogSensor = 2;
+        ads.startADCReading(ADS1X15_REG_CONFIG_MUX_SINGLE_2, false);
+        break;
+      case 2:
+        analogValues[2] =  ads.getLastConversionResults();
+        currentAnalogSensor = 3;
+        ads.startADCReading(ADS1X15_REG_CONFIG_MUX_SINGLE_3, false);
+        break;
+      case 3:
+        analogValues[3] =  ads.getLastConversionResults();
+        currentAnalogSensor = 0;
+        ads.startADCReading(ADS1X15_REG_CONFIG_MUX_SINGLE_0, false);
+        break;
+  }
+}
+
+void changeRecordingState() {
+  if(lastSaveTimeInSeconds + 1 > int(second())) {
+    return;
+  }
+  if(isRecording == true) {
+    while(digitalRead(7) == 1) {
+      Serial.println("RELEASE BUTTON");
+    }
+    outputFile.println(now());
+    outputFile.close();
+    digitalWrite(9, LOW); //turn off red LED
+    Serial.println("Data Recording Stopped");
+     
+    isRecording = false;
+  }
+  else {
+    while(digitalRead(7) == 1) {
+      Serial.println("RELEASE BUTTON");
+    }
+    String time =  String(year()) + "-" + String(month()) + "-" + String(day()) + " " + String(hour()) + "_" + String(minute()) + "_" + String(second());
+    Serial.println(time.c_str());
+    //turn on red LED
+    digitalWrite(9, HIGH);
+    outputFile = SD.open(time.c_str(),  FILE_WRITE);
+    outputFile.println(now());
+    isRecording = true;
+  }
+  lastSaveTimeInSeconds = int(second());
 }
