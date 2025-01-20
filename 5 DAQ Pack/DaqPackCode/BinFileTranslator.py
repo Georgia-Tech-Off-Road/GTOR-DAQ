658a92cead288787aad68ed14b59ca40adef5d8c
Andrew <50636418+Andrewbandrew05@users.noreply.github.com>
2025-01-20 18:52:40 -0500

    added second BNO05 sensor

--- 5 DAQ Pack/DaqPackCode/BinFileTranslator.py
+++ 5 DAQ Pack/DaqPackCode/BinFileTranslator.py
@@ -1,12 +1,12 @@
 import struct
 
 # Define the format for the binary data (including padding to match Arduino's structure size)
-data_format = "Q L 3i 4i 3f 3f 3f 4f 4i 4x"  # Add 4 bytes of padding (x4)
+data_format = "Q L 3i 4i 3f 3f 3f 4f 4i 3f 3f 3f 4f 4i"  # Add 4 bytes of padding (x4)
 data_size = struct.calcsize(data_format)
 
 # Main function
 def main():
-    input_file_name = "2025-1-19 22_17_35.bin"
+    input_file_name = "2025-1-20 18_37_34.bin"
     output_file_name = "output.txt"
 
     try:
