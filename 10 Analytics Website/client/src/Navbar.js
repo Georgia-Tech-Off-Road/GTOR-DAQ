de09d05373b432316199ccca4f3b87d656914aac
Alaric006 <alaricjenkins@outlook.com>
2025-01-24 19:00:06 -0500

    minor changes

---
+++ 10 Analytics Website/client/src/Navbar.js
@@ -0,0 +1,11 @@
+import './App.css'
+export default function Navbar() {
+    return (
+        <nav class="navbar">
+            <ul class="nav-list">
+                <li>Test Link 1</li>
+                <li>Test Link 2</li>
+            </ul>
+        </nav>
+    )
+}
