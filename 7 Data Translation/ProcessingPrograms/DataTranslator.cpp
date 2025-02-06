88849027df735e2cd4a949c1e269a94a658baa92
Alaric006 <alaricjenkins@outlook.com>
2025-02-05 21:57:09 -0500

    Implemented Config File Reader
    Created a function to read the .config file and create an array of initialized sensors from the file.

--- 7 Data Translation/ProcessingPrograms/DataTranslator.cpp
+++ 7 Data Translation/ProcessingPrograms/DataTranslator.cpp
@@ -5,32 +5,68 @@ using namespace std;
 //I use snake_casing for basic types, char, int, double, etc.
 //I use camelCasing for classes and booleans
 
-//Enum for SensorType
-enum DataType {digital = 0, analogue = 1};
+//If sensor layout changes, you can make a new struct and modfiy this alias. Orrr, (more dangerously) modify the current struct
+using Sensor = DAQSensor;
 
-//Define a struct that contains the basic data for each sensor
-struct Sensor {
-    int index;
-    DataType dataType;
-    std::string sensorName;
-    int pollingRate;
-    int numTeeth;
-};
+//  ---GLOBALS---
 
+//File IO
+string inputFileName;
+string outputFileName;
+string configFileName;
+
+ifstream in_file;
+ofstream out_file;
+ifstream config_file;
+
+//MISC
 bool verbose_flag = false;
+std::vector<Sensor> sensors;
+
 int main(int argc, char *argv[]) {
     LOG_INFO("You have entered %d cmd line arguments!\n", argc);
-    //Get name of input and output files
-    string inputFileName;
-    string outputFileName;
-    string configFileName;
 
+    processCLIArgs(argc, argv);
+    openFiles();
+    readConfigFile(&config_file);
+
+    //Before function termination
+    in_file.close();
+    out_file.close();
+    return 0;
+}
+
+void readConfigFile(ifstream *cf) {
+    const int CONFIG_BUFFER_SIZE = 70;
+    //Stores the text of ONE line from the file
+    char line_buffer[CONFIG_BUFFER_SIZE];
+
+    //Get rid of header
+    cf->getline(line_buffer, CONFIG_BUFFER_SIZE);
+
+    while (cf->getline(line_buffer, CONFIG_BUFFER_SIZE)) {
+        //Declare an empty Sensor
+        Sensor currSensor;
+
+        //Initialize the sensor given the options specified in the line
+        currSensor.initializeSensor(line_buffer);
+
+        //Push to back of vector
+        sensors.push_back(currSensor);
+    }
+
+    for(Sensor sensor : sensors) {
+        cout << sensor.toStr().c_str();
+    }
+    config_file.close();
+}
+
+//Self explanatory function name
+void processCLIArgs(int argc, char *argv[]) {
     //Flags
     bool assignedInput = false;
     bool assignedOutput = false;
     bool assignedConfig = false;
-
-
 
     //Get input and output file names
     for (int i = 1; i < argc; i++) {
@@ -52,23 +88,23 @@ int main(int argc, char *argv[]) {
         }
     }
     if(!assignedInput || !assignedOutput || !assignedConfig) {
-        fprintf(stderr, "You must provide an input and output file in this format: ./dataprocess -i input.txt -o output.csv -c config.txt\n");
-        return -1;
+        throw std::invalid_argument("You must provide an input and output file in this format: ./dataprocess -i input.txt -o output.csv -c config.txt\n");
     }
     LOG_INFO("Verbose: %s\n", verbose_flag ? "true" : "false");
     LOG_INFO("Running dataprocess with following parameters:\n");
     LOG_INFO("Input file: %s\n", inputFileName.c_str());
     LOG_INFO("Output file: %s\n", outputFileName.c_str());
     LOG_INFO("Config file name: %s\n", configFileName.c_str());
+}
 
+//Opens input, output, and config files
+void openFiles() {
     //Open input file in read mode
-    ifstream in_file(inputFileName, std::ios_base::in);
-
-    //Open output file in read mode
-    ofstream out_file(outputFileName, std::ios_base::out);
-
+    in_file.open(inputFileName, std::ios_base::in);
+    //Open output file in write mode
+    out_file.open(outputFileName, std::ios_base::out);
     //Open config file in read mode
-    ifstream config_file(configFileName, std::ios_base::in);
+    config_file.open(configFileName, std::ios_base::in);
 
     //Check to see if input file is open
     if(!in_file.is_open()) {
@@ -84,23 +120,4 @@ int main(int argc, char *argv[]) {
     if(!config_file.is_open()) {
         LOG_ERROR("Config file %s could not be opened!", configFileName.c_str());
     }
-
-    readConfigFile(&config_file);
-
-    //Before function termination
-    in_file.close();
-    out_file.close();
-    config_file.close();
-    return 0;
 }
-
-void readConfigFile(ifstream *cf) {
-    const int CONFIG_BUFFER_SIZE = 70;
-    char line_buffer[CONFIG_BUFFER_SIZE];
-    //Get rid of header
-    (*cf).getline(line_buffer, CONFIG_BUFFER_SIZE);
-    (*cf).getline(line_buffer, CONFIG_BUFFER_SIZE);
-    LOG_INFO("Line from config file: %s\n", line_buffer);
-    
-}
-
