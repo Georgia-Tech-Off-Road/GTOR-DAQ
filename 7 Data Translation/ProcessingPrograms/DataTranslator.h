15a01a6570d8fcf4a0058edfa12d10ec0018a343
Alaric Jenkins <alaricjenkins@outlook.com>
2025-02-23 19:09:52 -0500

    Refined column sensor mismatch error message thrown

--- 7 Data Translation/ProcessingPrograms/DataTranslator.h
+++ 7 Data Translation/ProcessingPrograms/DataTranslator.h
@@ -24,6 +24,9 @@
 
 //Buffer size for each line read from input file, should be much larger than expected line-length, no resizing!
 inline constexpr int LINE_BUFFER_SIZE = 1000;
+
+//Global Line Number
+int current_line_number = 0;
 //If you're confused why constexpr is used instead of const, read here: https://www.cppstories.com/2022/const-options-cpp20/
 
 void readConfigFile(std::ifstream *cf);
