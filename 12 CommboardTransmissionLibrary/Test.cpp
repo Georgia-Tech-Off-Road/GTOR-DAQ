8b8ef8aa184a9b6859e99ce3e45dac5df8be8053
Alaric Jenkins <alaricjenkins@outlook.com>
2025-03-13 21:45:45 -0400

    Save commit

--- 12 CommboardTransmissionLibrary/Test.cpp
+++ 12 CommboardTransmissionLibrary/Test.cpp
@@ -2,9 +2,11 @@
 #include "Sensors.h"
 #include <cstdio>
 int main() {
-	boost::endian::big_uint32_t example = 0x01020304;
-	boost::endian::little_uint32_t example_2 = 0x01020304;
-	printf("Comparison: %x %x\n", example, example_2);
+	FILE* fi = fopen("test_output.dat", "wb");
+	uint32_t test = 0xAAAAFFFF;
+	boost::endian::big_uint32_t example = 0xAAAAFFFF;
+	boost::endian::little_uint32_t example_2 = 0xAAAAFFFF;
+	printf("Comparison 1: %x %xs\n", *(&example), *(&example_2));
 	std::bitset<32> encoded_example;
 	cmbtl::misc::copyBits(&encoded_example, example);
 	std::bitset<32> encoded_example_2;
--- 12 CommboardTransmissionLibrary/misc/CopyBits.h
+++ 12 CommboardTransmissionLibrary/misc/CopyBits.h
@@ -1,6 +1,7 @@
 #include <cstring>
 #include <bitset>
 #include <assert.h>
+#include <iostream>
 
 #ifndef CMBTL_COPY_BITS_H
 #define CMBTL_COPY_BITS_H
@@ -12,16 +13,23 @@ namespace cmbtl {
 			assert((sizeof(src) * 8) >= N && "Your encoded value takes more bits than the size of your stored value!");
 			//Clear all bits
 			dest->reset();
-			char* buffer = static_cast<char*>(malloc(sizeof(src)));
-			memcpy(buffer, &src, sizeof(src));
-			for (uint16_t i = 0; i < dest->size(); i++) {
-				//ith bit in src is 1
-				if (((buffer[i/(sizeof(char) * 8)] >> i) & 0x1) == 1) {
+			//Create a buffer to copy the bits directly to.
+			//We can't use >> operators to shift and get bits from the SV because the >> is overloaded for
+			//both big and little endian integers and floats. Basically bigEndian >> 1 and littleEndian >> 1 will shift
+			//towards the least significant bit. Great for most things, but we just want to copy bits! So we need to use a buffer.
+			char buffer[sizeof(src) / sizeof(char)];
+			memcpy(buffer, &src, sizeof(src)); 
+			for (uint16_t i = dest->size() - 1; i > 0; i--) {
+				//First get which char our desired bit is in, then bit shifts to place the bit in the least significant position,
+				// and finally masks out every other bit.
+				int bufferIndex = (i / (sizeof(char) * 8));
+				int shift_ammount = ((-i + (dest->size() - 1)) % (sizeof(char) * 8));
+				uint8_t bit = buffer[bufferIndex] >> shift_ammount & 0x1;
+				if (bit == 1) {
 					//Set bit
-					(*dest)[i] = 1;
+					(*dest)[-i + (dest->size() - 1)] = 1;
 				}
 			}
-			free(buffer);
 		}
 		//SV: Stored Value
 		template <std::size_t N, typename SV>
