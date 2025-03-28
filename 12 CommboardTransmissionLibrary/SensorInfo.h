58cec1032b82774335f32a6f8c925402d0fb1108
Alaric Jenkins <alaricjenkins@outlook.com>
2025-03-27 23:12:04 -0400

    Updated SensorInfo to be a fully static class
    Also millisec sensor

--- 12 CommboardTransmissionLibrary/SensorInfo.h
+++ 12 CommboardTransmissionLibrary/SensorInfo.h
@@ -6,37 +6,23 @@ namespace cmbtl {
     //SV: Stored Value, value that is stored in the Data struct
     //RV: Real Value, value that we would commonly use this as. Ex. temp is stored most efficiently as a uint (we get that from the ADC),
     //      it will be encoded and decoded as a uint, but is proabably most useful as a float afterwards
-    template <typename SV, typename RV>
-    struct SensorInfo {
-        using STORED_VALUE = SV;
-        using REAL_VALUE = RV;
-        //Numbers of bits that the SV will be stored as in the buffer
-        uint32_t ENCODED_BIT_SIZE;
-        //Takes data from data and stores it in buffer
-        void (*encode)(SV data, BinaryBuffer& buffer);
-        //Takes data from buffer and updates data
-        SV (*decode)(const BinaryBuffer& buffer);
-        //Convert to a more natural (and probably less space efficient) data type after data is sent over.
-        RV(*convert)(SV);
-
-        //We use an initializer list for constexpr to work
-        constexpr SensorInfo(uint32_t bit_size, 
-            void (*enc)(SV data, BinaryBuffer& buffer), 
-            SV (*dec)(const BinaryBuffer& buffer), 
-            RV(*conv)(SV)) : ENCODED_BIT_SIZE(bit_size), 
-            encode(enc),
-            decode(dec), 
-            convert(conv) {}
-    };
-
     template <typename SV, 
     typename RV, 
     uint32_t BIT_SIZE,
     void (*ENCODE)(SV data, BinaryBuffer& buffer),
     SV (*DECODE)(const BinaryBuffer& buffer),
     RV (*CONVERT)(SV)>
-    struct CompileTimeSensorInfo {
-        static constexpr SensorInfo<SV, RV> value{BIT_SIZE, ENCODE, DECODE, CONVERT};
-    };
+    struct SensorInfo {
+        using STORED_VALUE = SV;
+        using REAL_VALUE = RV;
+        //Numbers of bits that the SV will be stored as in the buffer
+        static constexpr ENCODED_BIT_SIZE = BIT_SIZE;
+        //Takes data from data and stores it in buffer
+        static constexpr void (*encode)(SV data, BinaryBuffer& buffer) = ENCODE;
+        //Takes data from buffer and updates data
+        static constexpr (*decode)(const BinaryBuffer& buffer) = DECODE;
+        //Convert to a more natural (and probably less space efficient) data type after data is sent over.
+        static constexpr RV(*convert)(SV) = CONVERT;
+    }
 }
 #endif
