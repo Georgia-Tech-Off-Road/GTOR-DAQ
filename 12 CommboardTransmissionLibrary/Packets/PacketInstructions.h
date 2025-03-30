f0f45def22ff96904434e888e352df608dd2bb0f
Alaric Jenkins <alaricjenkins@outlook.com>
2025-03-29 21:57:53 -0400

    Refractored PacketInstructions.h

--- 12 CommboardTransmissionLibrary/Packets/PacketInstructions.h
+++ 12 CommboardTransmissionLibrary/Packets/PacketInstructions.h
@@ -1,25 +1,9 @@
-#include "misc/BinaryBuffer/BinaryBuffer.h"
+#include "../misc/BinaryBuffer/BinaryBuffer.h"
 #include "Sensors.h"
 #ifndef CMBTL_PACKET_INSTRUCTIONS_H
 #define CMBTL_PACKET_INSTRUCTIONS_H
 namespace cmbtl {
-    class PacketInstructions : BinaryBuffer {
-        public:
-            inline PacketInstructions() {
-                BinaryBuffer(cmbtl::NUM_SENSORS);
-                uint32_t i = 0;
-            }
-            inline uint32_t getPacketEncodedBitSize() const {
-                uint32_t bit_size = 0;
-                uint32_t i = 0;
-                for (SensorInfo SENSOR : SENSORS) {
-                    if (getBit(i) == 1) {
-                        bit_size += SENSOR.ENCODED_BIT_SIZE;
-                    }
-                    i++;
-                }
-                return bit_size;
-            }
-    }
+    template <size_t N>
+    using PacketInstructions = std::array<bool, N>;
 }
 #endif
