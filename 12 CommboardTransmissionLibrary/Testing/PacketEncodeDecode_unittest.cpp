b752e523ccd13a79fd95ef4932f539fbdae7dc86
Alaric Jenkins <alaricjenkins@outlook.com>
2025-04-01 23:19:39 -0400

    Redefined PacketInstructions
    Made PacketInstructions inherit from std::bitset rather than from binary buffer

--- 12 CommboardTransmissionLibrary/Testing/PacketEncodeDecode_unittest.cpp
+++ 12 CommboardTransmissionLibrary/Testing/PacketEncodeDecode_unittest.cpp
@@ -1,9 +1,2 @@
 #include "gtest/gtest.h"
 #include "PacketEncodeDecode.h"
-
-TEST(PacketInstructionsTest, EncodedPacketBitSize) {
-    using SensorInfoTuple = std::tuple<cmbtl::millisec::MILLI_SEC_SENSOR_INFO>;
-
-    uint32_t packetSize = cmbtl::EncodedPacketBitSize<cmbtl::PacketInstructions<true>, SensorInfoTuple>::value;
-    ASSERT_EQ(packetSize, 26);
-}
