238559deb2031b52b817014572e27d5baf832b5e
Alaric Jenkins <alaricjenkins@outlook.com>
2025-04-05 22:44:57 -0400

    Tentative implementation of CVTTempSensor Translation

--- 7 Data Translation/ConversionLibraries/ConversionFunctions.h
+++ 7 Data Translation/ConversionLibraries/ConversionFunctions.h
@@ -1,8 +1,7 @@
 #pragma once
-namespace cvf {
-    #include "./BrakePressure/BrakePressureConvert.h"
-    #include "./Time/TimeConvert.h"
-    #include "./RPM/RPMConvert.h"
-    #include "./Copy/CopyConvert.h"
-    #include "./CVTTempSensor/CVTTempSensor.h"
-}
+
+#include "./BrakePressure/BrakePressureConvert.h"
+#include "./Time/TimeConvert.h"
+#include "./RPM/RPMConvert.h"
+#include "./Copy/CopyConvert.h"
+#include "./CVTTempSensor/CVTTempSensor.h"
