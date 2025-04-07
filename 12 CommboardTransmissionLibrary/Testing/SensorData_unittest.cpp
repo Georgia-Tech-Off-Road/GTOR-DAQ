b8c3f9078b1ecb9b5e618f90fdc64dfb83f62cda
Alaric Jenkins <alaricjenkins@outlook.com>
2025-04-06 22:05:08 -0400

    Added defaultEncode(), defaultDecode(), and defaultConvert() functions in SensorInfo

--- 12 CommboardTransmissionLibrary/Testing/SensorData_unittest.cpp
+++ 12 CommboardTransmissionLibrary/Testing/SensorData_unittest.cpp
@@ -90,34 +90,6 @@ TEST(SensorDataTests, runtimeDecodeData) {
     ASSERT_EQ(actual, 359);
 }
 
-TEST(SensorDataTests, encodePacket) {
-    SensorDataType sensorData;
-    PacketInstructions<SensorDataType::NUM_SENSORS> instructions;
-    instructions.set(0);
-    sensorData.setData<0>(427);
-
-    BinaryBuffer buffer = sensorData.encodePacket(instructions);
-    
-    cmbtl::millisec::MILLI_SEC_SENSOR_INFO::STORED_VALUE actual = buffer.readValue<cmbtl::millisec::MILLI_SEC_SENSOR_INFO::STORED_VALUE>(cmbtl::millisec::MILLI_SEC_SENSOR_INFO::ENCODED_BIT_SIZE);
-    ASSERT_EQ(actual, 427);
-
-    instructions.set(0, false);
-    ASSERT_TRUE(sensorData.encodePacket(instructions).getCapacity() == 0);
-
-}
-
-TEST(SensorDataTests, decodePacket) {
-    SensorDataType sensorData;
-    PacketInstructions<SensorDataType::NUM_SENSORS> instructions;
-    instructions.set(0);
-
-    BinaryBuffer buffer(cmbtl::millisec::MILLI_SEC_SENSOR_INFO::ENCODED_BIT_SIZE);
-    buffer.writeValue<cmbtl::millisec::MILLI_SEC_SENSOR_INFO::STORED_VALUE>(451, cmbtl::millisec::MILLI_SEC_SENSOR_INFO::ENCODED_BIT_SIZE);
-
-    sensorData.decodePacket(instructions, buffer);
-
-    ASSERT_EQ(sensorData.getData<0>(), 451);
-}
 
 TEST(SensorDataTests, runtimeEncodeAndDecodeData) {
     SensorDataType sensorData;
