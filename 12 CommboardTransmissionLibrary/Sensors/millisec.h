1ccca8ed3d2b8c81ebc9d57fbbbc3d9e1cc5525f
Alaric Jenkins <alaricjenkins@outlook.com>
2025-04-08 19:17:39 -0400

    Added clutch engaged and pit requested sensor infos

--- 12 CommboardTransmissionLibrary/Sensors/millisec.h
+++ 12 CommboardTransmissionLibrary/Sensors/millisec.h
@@ -3,7 +3,6 @@
 #include "../boost/endian/arithmetic.hpp"
 #include "../boost/endian/conversion.hpp"
 #include <cstdint>
-#include <bitset>
 #ifndef CMBTL_MILLIS_H
 #define CMBTL_MILLIS_H
 namespace cmbtl {
@@ -16,19 +15,6 @@ namespace cmbtl {
 
 		//'Real Value' of millisec
 		using RV = uint32_t;
-
-		inline void encodeMillisec(SV value, BinaryBuffer& buffer) {
-			buffer.writeValue(value, ENCODED_BIT_SIZE);
-		}
-
-		inline SV decodeMillisec(const BinaryBuffer& buffer) {
-			return buffer.readValue<SV>(ENCODED_BIT_SIZE);
-		}
-
-		inline RV convert(SV stored_val) {
-			uint32_t real_value = stored_val;
-			return real_value;
-		}
 
 		//The millisec sensor
 		using MILLI_SEC_SENSOR_INFO = cmbtl::SensorInfo<SV, RV, ENCODED_BIT_SIZE>;
