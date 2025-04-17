4278e7f35a967c0a41cae2aec337a81e4eed5b08
Alaric Jenkins <alaricjenkins@outlook.com>
2025-04-17 19:19:17 -0400

    Added serializeToJSON for sensors
    Added serializeToJSON for millisec, clutch_engaged, and pit_requested sensors. Also enhanced DefaultBoolSensor<> and added DefaultSensor<> templates

--- 12 CommboardTransmissionLibrary/Sensors/millisec.h
+++ 12 CommboardTransmissionLibrary/Sensors/millisec.h
@@ -3,6 +3,8 @@
 #include "../boost/endian/arithmetic.hpp"
 #include "../boost/endian/conversion.hpp"
 #include <cstdint>
+#include <string>
+#include <sstream>
 #ifndef CMBTL_MILLIS_H
 #define CMBTL_MILLIS_H
 namespace cmbtl {
@@ -16,8 +18,12 @@ namespace cmbtl {
 		//'Real Value' of millisec
 		using RV = uint32_t;
 
+		void serializeToJSON(const RV& convertedVal, std::stringstream& ss) {
+			ss << "millisec: " << convertedVal;
+		}
+
 		//The millisec sensor
-		using MILLI_SEC_SENSOR_INFO = cmbtl::SensorInfo<SV, RV, ENCODED_BIT_SIZE>;
+		using MILLI_SEC_SENSOR_INFO = DefaultSensor<SV, RV, ENCODED_BIT_SIZE, serializeToJSON>;
 	}
 }
 #endif
--- 12 CommboardTransmissionLibrary/Sensors/pit_requested.h
+++ 12 CommboardTransmissionLibrary/Sensors/pit_requested.h
@@ -3,7 +3,12 @@
 #define CMBTL_PIT_REQUESTED_H
 namespace cmbtl {
     namespace pit_requested {
-        using PIT_REQUESTED_SENSOR_INFO = DefaultBoolSensorInfo;
+
+        void serializeToJSON(const bool& convertedVal, std::stringstream& ss) {
+            ss << "pit_requested: " << (convertedVal ? "true" : "false");
+        }
+
+        using PIT_REQUESTED_SENSOR_INFO = DefaultBoolSensorInfo<serializeToJSON>;
     }
 }
 #endif
