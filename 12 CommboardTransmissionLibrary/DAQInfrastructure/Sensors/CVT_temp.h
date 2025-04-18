1245a92186273a26e556d089f37bec5939114798
Alaric Jenkins <alaricjenkins@outlook.com>
2025-04-18 11:35:21 -0400

    Implemeneted serializeToJSON for all sensors

--- 12 CommboardTransmissionLibrary/DAQInfrastructure/Sensors/CVT_temp.h
+++ 12 CommboardTransmissionLibrary/DAQInfrastructure/Sensors/CVT_temp.h
@@ -1,6 +1,8 @@
 #include "../../SensorInfo.h"
 #include "../../boost/endian/arithmetic.hpp"
 #include "../../misc/BinaryBuffer/BinaryBuffer.h"
+#include <string>
+#include <sstream>
 #ifndef CMBTL_CVT_TEMP_H
 #define CMBTL_CVT_TEMP_H
 
@@ -11,7 +13,11 @@ namespace cmbtl {
         using SV = boost::endian::big_uint16_t;
         using RV = uint16_t;
 
-        using CVT_TEMP_SENSOR_INFO = SensorInfo<SV, RV, ENCODED_BIT_SIZE>;
+        inline void serializeToJSON(const RV& convertedVal, std::stringstream& ss) {
+            ss << "\"CVTTemp: \"" << convertedVal;
+        }
+
+        using CVT_TEMP_SENSOR_INFO = DefaultSensor<SV, RV, ENCODED_BIT_SIZE, serializeToJSON>;
     }
 }
 
--- 12 CommboardTransmissionLibrary/DAQInfrastructure/Sensors/brake_temp.h
+++ 12 CommboardTransmissionLibrary/DAQInfrastructure/Sensors/brake_temp.h
@@ -1,20 +1,54 @@
 #include "../../SensorInfo.h"
 #include "../../boost/endian/arithmetic.hpp"
 #include "../../misc/BinaryBuffer/BinaryBuffer.h"
+#include <string>
+#include <sstream>
 
 #ifndef CMBTL_BRAKE_TEMP_H
 #define CMBTL_BRAKE_TEMP_H
 
 namespace cmbtl {
     namespace brake_temp {
-        constexpr uint32_t ENCODED_BIT_SIZE = 16;
+        constexpr uint32_t AXIS_ENCODED_BIT_SIZE = 16;
 
-        using SV = boost::endian::big_uint16_t;
-        using RV = uint16_t;
+        using AXIS_SV = boost::endian::big_uint16_t;
 
-        using BRAKE_TEMP_FRONT_LEFT_SENSOR_INFO = SensorInfo<SV, RV, ENCODED_BIT_SIZE>;
-        using BRAKE_TEMP_FRONT_RIGHT_SENSOR_INFO = SensorInfo<SV, RV, ENCODED_BIT_SIZE>;
-        using BRAKE_TEMP_BACK_SENSOR_INFO = SensorInfo<SV, RV, ENCODED_BIT_SIZE>;
+        constexpr uint32_t ENCODED_BIT_SIZE = AXIS_ENCODED_BIT_SIZE * 3;
+
+        struct BrakeTemp {
+            AXIS_SV front_left;
+            AXIS_SV front_right;
+            AXIS_SV rear;
+        };
+
+        using SV = BrakeTemp;
+        using RV = BrakeTemp;
+
+        inline void encodeBrakeTemp(const SV& val, BinaryBuffer& buffer) {
+            buffer.writeValue<AXIS_SV>(val.front_left, AXIS_ENCODED_BIT_SIZE);
+            buffer.writeValue<AXIS_SV>(val.front_right, AXIS_ENCODED_BIT_SIZE);
+            buffer.writeValue<AXIS_SV>(val.rear, AXIS_ENCODED_BIT_SIZE);
+        }
+
+        inline SV decodeBrakeTemp(const BinaryBuffer& buffer) {
+            BrakeTemp data;
+
+            data.front_left = buffer.readValue<AXIS_SV>(AXIS_ENCODED_BIT_SIZE);
+            data.front_right = buffer.readValue<AXIS_SV>(AXIS_ENCODED_BIT_SIZE);
+            data.rear = buffer.readValue<AXIS_SV>(AXIS_ENCODED_BIT_SIZE);
+
+            return data;
+        }
+
+        inline void serializeToJSON(const RV& val, std::stringstream& ss) {
+            ss << "\"brakeTemp\": " << "{";
+            ss << "\"frontLeft\": " << val.front_left << ", ";
+            ss << "\"frontRight\": " << val.front_right << ", ";
+            ss << "\"rear\": " << val.rear;
+            ss << "}";
+        }
+
+        using BRAKE_TEMP_SENSOR_INFO = SensorInfo<SV, RV, ENCODED_BIT_SIZE, encodeBrakeTemp, decodeBrakeTemp, defaultConvert<SV, RV>, serializeToJSON>;
     }
 }
 
