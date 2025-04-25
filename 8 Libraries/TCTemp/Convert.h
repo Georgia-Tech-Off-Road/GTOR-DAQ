e0e3d08f9b3e4926dd133cb9b861dc59c1f902de
Alaric Jenkins <alaricjenkins@outlook.com>
2025-04-25 19:03:21 -0400

    TCTemp Implementation in progress

--- 8 Libraries/TCTemp/Convert.h
+++ 8 Libraries/TCTemp/Convert.h
@@ -1,38 +1,28 @@
 #ifndef TC_TEMP_CONVERT_H
 #define TC_TEMP_CONVERT_H
 #include <cstdint>
+#include <cmath>
 
-namespace CVTBeltTemp {
+namespace TCTemp {
 
-    /**
-     * @brief Converts ADC value of CVTTemp sensor to temperature value (°C)
-     * 
-     * @param ADC_val: Value retrieved from the ADC (currently 15 bits)
-     * 
-     * @returns temperature of the tranfer case (°C)
-     */
-    inline float calculateTemp(uint16_t ADC_val) {
+    float calculateR2(float V_s, float V_out, float R_1) {
+        float R_2 = R_1 / ((V_s / V_out) - 1);
 
+        return R_2;
     }
 
-    /**
-     * @brief Converts value from the ADC to mV
-     * 
-     * @param ADC_val: Value retrieved from the ADC (Currently 15 bits)
-     * 
-     * @returns converted value in mV
-     */
-    inline float convertToMilliVolts(uint16_t ADC_val) {
-        // Maximum value is 0111 1111 1111 1111 (15 bits) = 2^16 - 1 - 2^15
-        const uint16_t MAX_ADC_VALUE = 32767;
+    float calculateTemp(uint16_t ADC_val) {
 
-        // Maximum voltage output from sensor is 5000 mV
-        const float MAX_VOLTAGE = 5000.0f;
+        float R_2 = calculateR2(ADC_val);
 
-        // Minimu voltage output from sensor is 0 mV
-        const float MIN_VOLTAGE = 0.0f;
+        // Define coefficients
+        const int a = -1750;
+        const int b = 0.1;
+        const int c = 2212;
 
-        return static_cast<float>(ADC_val) * ((MAX_VOLTAGE - MIN_VOLTAGE) / static_cast<float>(MAX_ADC_VALUE));
+        float temp = a * std::pow(std::log(R_2), static_cast<float>(b)) + c;
+
+        return temp;
     }
 }
 
