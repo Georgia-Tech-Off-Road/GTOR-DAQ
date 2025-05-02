93bdfb5c35a3c5f2ec982be9e6d9abbc3a0ff6b4
Alaric Jenkins <alaricjenkins@outlook.com>
2025-05-02 08:39:48 -0700

    Refactoring ConvertToVolts from other libraries
    Removing ConvertToVolts functionality to its own ADC library for modularity

--- 8 Libraries/TCTemp/Convert.h
+++ 8 Libraries/TCTemp/Convert.h
@@ -1,45 +1,23 @@
 #ifndef TC_TEMP_CONVERT_H
 #define TC_TEMP_CONVERT_H
+#include <functional>
 #include <cstdint>
 #include <cmath>
 
 namespace TCTemp {
-
-    /**
-     * @brief Converts value from the ADC to mV
-     * 
-     * @param ADC_val: Value retrieved from the ADC (Currently 15 bits)
-     * 
-     * @returns converted value in mV
-     */
-    inline float convertToVolts(uint16_t ADC_val) {
-        // Maximum value is 0111 1111 1111 1111 (15 bits) = 2^16 - 1 - 2^15
-        const uint16_t MAX_ADC_VALUE = 32767;
-
-        // Maximum voltage output from sensor is 5 V
-        const float MAX_VOLTAGE = 6.144f;
-
-        // Minimu voltage output from sensor is 0 V
-        const float MIN_VOLTAGE = 0.0f;
-
-        return static_cast<float>(ADC_val) * ((MAX_VOLTAGE - MIN_VOLTAGE) / static_cast<float>(MAX_ADC_VALUE));
-    }
-
     inline float calculateR2(float V_s, float V_out, float R_1) {
         float R_2 = ((V_out / V_s) * R_1) / (1 - (V_out / V_s));
 
         return R_2;
     }
 
-    inline float calculateTemp(uint16_t ADC_val) {
-
+    inline float calculateTemp(float V_out) {
         // Define constants
         const float V_S = 5;
         const float R_1 = 2200;
-        
-        float V_out = convertToVolts(ADC_val);
-        float R_2 = calculateR2(V_S, V_out, R_1);
 
+        float R_2 = calculateR2(V_S, V_out, R_1);
+ 
         // Define coefficients
         const int a = -1750;
         const int b = 0.1;
