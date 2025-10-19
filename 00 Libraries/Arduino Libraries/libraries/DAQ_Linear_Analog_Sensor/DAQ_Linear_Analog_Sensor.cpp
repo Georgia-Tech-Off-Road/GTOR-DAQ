c69d65269fe52450e5ffd3bfc49c6df67efa2733
Alaric006 <alaricjenkins@outlook.com>
2025-10-19 19:41:59 -0400

    Tentative Changes to add Recent Recording flag
    Could not compile code on computer due to library conflicts and CMBTL
    issues.

--- 00 Libraries/Arduino Libraries/libraries/DAQ_Linear_Analog_Sensor/DAQ_Linear_Analog_Sensor.cpp
+++ 00 Libraries/Arduino Libraries/libraries/DAQ_Linear_Analog_Sensor/DAQ_Linear_Analog_Sensor.cpp
@@ -1,5 +1,6 @@
 #include "DAQ_Linear_Analog_Sensor.h"
 #include "Arduino.h"
+#include <TimeLib.h>
 
 Linear_Analog_Sensor :: Linear_Analog_Sensor(uint8_t resolution, float referenceVoltage, float maximalValue, float minimalValue, float maximalVoltage, float minimalVoltage) {
     //initialize private variables
@@ -9,6 +10,8 @@ Linear_Analog_Sensor :: Linear_Analog_Sensor(uint8_t resolution, float reference
     _minimalVoltage = minimalVoltage;
     _resolution = resolution;
     _referenceVoltage = referenceVoltage;
+    _recentRecording = false;
+    _previousRecordingMS = 0;
 }
 
 float Linear_Analog_Sensor :: computeVoltage(int reading) { 
@@ -17,8 +20,22 @@ float Linear_Analog_Sensor :: computeVoltage(int reading) {
 }
 
 float Linear_Analog_Sensor :: computeSensorReading(int reading) {
+    // Update if recording is recent or not
+    updateRecentRecording();
+    _previousRecordingMS = currentRecordingMS;
     float voltage = computeVoltage(reading);
     float value = (voltage - _minimalVoltage)/(_maximalVoltage - _minimalVoltage) * (_maximalValue - _minimalValue);
     return value;
 }
 
+bool Linear_Analog_Sensor::hasRecentRecording() {
+    return _recentRecording;
+}
+
+void updateRecentRecording() {
+    _recentRecording = false;
+    uint32_t currentRecordingMS = millis();
+    if (currentRecordingMS - _previousRecordingMS <= DAQ_ANALOG_SENSOR_RECENT_RECORDING_TIME) {
+        _recentRecording = true;
+    }
+}
--- 00 Libraries/Arduino Libraries/libraries/DAQ_Linear_Analog_Sensor/DAQ_Linear_Analog_Sensor.h
+++ 00 Libraries/Arduino Libraries/libraries/DAQ_Linear_Analog_Sensor/DAQ_Linear_Analog_Sensor.h
@@ -1,6 +1,9 @@
 #ifndef DAQ_Linear_Analog_Sensor_H
 #define DAQ_Linear_Analog_Sensor_H
 
+// Defines a time difference that defines if there has been a recent recording
+#define DAQ_ANALOG_SENSOR_RECENT_RECORDING_TIME 10
+
 #include "DAQ_Linear_Analog_Sensor.h"
 
 #include <Arduino.h>
@@ -11,6 +14,7 @@ class Linear_Analog_Sensor {
         Linear_Analog_Sensor(uint8_t resolution, float referenceVoltage, float maximalValue, float minimalValue, float maximalVoltage, float minimalVoltage);
         float computeVoltage(int reading);
         float computeSensorReading(int reading);
+        bool hasRecentRecording();
     private:
         uint8_t _resolution;
         float _referenceVoltage;
@@ -18,6 +22,13 @@ class Linear_Analog_Sensor {
         float _minimalValue;
         float _maximalVoltage;
         float _minimalVoltage;
+        // Debug boolean if sensor reading has been computed recently based on
+        bool _recentRecording;
+        // Time of previous recording in millisec
+        uint32_t previousRecordingMS;
+
+        void updateRecentRecording();
+
         
 };
 
